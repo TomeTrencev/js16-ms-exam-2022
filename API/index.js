b15bbb30d3d50a12956b4f811617cf8068e2a7bf
unknown <trencevtome@yahoo.com>
2022-04-06 21:16:34 +0200

    update6

--- API/index.js
+++ API/index.js
@@ -4,3 +4,16 @@ require('./pkg/db');
 const express = require('express');
 const jwt = require('express-jwt');
 const auth = require('./handlers/auth');
+
+const api = express();
+
+api.use(express.json());
+api.use(jwt({
+    secret: config.get('service').jwt_key,
+    algorithms: ['HS256']
+}).unless({
+    path: [
+       
+    ]
+}));
+
